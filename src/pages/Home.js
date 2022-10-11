c069a2125073f5684ba54fc8d90e9707d61e6a91
Fernando Barreto <fernando.gbarreto@hotmail.com>
2022-10-11 17:39:48 -0300

    testes do requisito 08

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 import './Home.css';
 
@@ -10,6 +11,8 @@ class Home extends React.Component {
       productList: [],
       categories: [],
       query: '',
+      id: '',
+      cartItems: [],
       category: '',
       buttonClicked: false,
     };
@@ -22,11 +25,25 @@ class Home extends React.Component {
 
   handleChange = ({ target: { name, value } }) => this.setState({ [name]: value });
 
+  addCartItem = () => {
+    const { productList, id } = this.state;
+    const product = productList.find((ele) => ele.id === id);
+    console.log(product);
+    this.setState((prevState) => ({
+      cartItems: [...prevState.cartItems, product],
+    }));
+  };
+
   handleSearch = async () => {
     const { query, category } = this.state;
     const response = await getProductsFromCategoryAndQuery(category, query);
     const productList = response ? response.results : [];
     this.setState({ productList, buttonClicked: true });
+  };
+
+  cartClick = ({ target }) => {
+    const { name } = target;
+    this.setState({ id: name }, this.addCartItem);
   };
 
   handleClick = ({ target }) => {
@@ -36,6 +53,8 @@ class Home extends React.Component {
 
   render() {
     const { productList, categories, buttonClicked } = this.state;
+    const { history: { push } } = this.props;
+    console.log(push);
     return (
       <main>
         <nav>
@@ -81,23 +100,34 @@ class Home extends React.Component {
           {
             productList.length
               ? (
-                productList.map(({ id, title, thumbnail, price }) => (
-                  <Link
-                    to={ `product/${id}` }
-                    key={ id }
-                    data-testid="product-detail-link"
-                  >
-                    <div data-testid="product" className="product-card">
-                      <span>{title}</span>
-                      <img src={ thumbnail } alt={ title } />
-                      <span>{price}</span>
-                    </div>
-                  </Link>
+                productList.map(({ id, title, thumbnail, price }, index) => (
+                  <div key={ index }>
+                    <Link
+                      to={ `product/${id}` }
+                      key={ id }
+                      data-testid="product-detail-link"
+                    >
+                      <div data-testid="product" className="product-card" name={ id }>
+                        <span>{title}</span>
+                        <img src={ thumbnail } alt={ title } />
+                        <span>{price}</span>
+                      </div>
+                    </Link>
+                    <button
+                      name={ id }
+                      type="button"
+                      onClick={ this.cartClick }
+                      data-testid="product-add-to-cart"
+                    >
+                      Adicionar ao carrinho
+
+                    </button>
+                  </div>
                 )))
               : buttonClicked && <span>Nenhum produto foi encontrado</span>
           }
         </section>
-        <Link to="/shoppingcart">
+        <Link to={ { pathname: 'shoppingcart', state: this.state } }>
           <button type="button" data-testid="shopping-cart-button">
             Carrinho de Compras
           </button>
@@ -106,5 +136,8 @@ class Home extends React.Component {
     );
   }
 }
+
+Home.propTypes = {
+  history: PropTypes.object }.isRequired;
 
 export default Home;
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class ShoppingCart extends React.Component {
   render() {
+    const { history: { location } } = this.props;
+    const { state: { cartItems } } = location;
     return (
-      <div>
+      <main>
+        <div>
+          <p data-testid="shopping-cart-product-quantity">{cartItems.length}</p>
+          <ul>
+            {cartItems.map((product, index) => (
+              <li
+                key={ index }
+                data-testid="shopping-cart-product-name"
+              >
+                {product.title}
+              </li>
+
+            ))}
+          </ul>
+        </div>
         <ul>
           <li><Link to="/">Home</Link></li>
         </ul>
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-      </div>
+      </main>
     );
   }
 }
+
+ShoppingCart.propTypes = {
+  history: PropTypes.object }.isRequired;
 
 export default ShoppingCart;
